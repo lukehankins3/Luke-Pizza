60dca884228ac96566e236c43e3156da844d5013
Luke <luke_hankins1@baylor.edu>
2022-09-29 14:59:58 -0500

    hw

---
+++ HW10/PayrollDeductionClass.py
@@ -0,0 +1,18 @@
+class PayrollDeduction:
+    def __init__(self,desc,date,amt,num):
+        self.__desc = desc
+        self.__date = date
+        self.__amt = amt
+        self.__num = num
+
+        def get_desc(self):
+            return self.__desc
+        
+        def get_date(self):
+            return self.__date
+
+        def get_amt(self):
+            return self.__amt
+            
+        def get_num(self):
+            return self.__num
